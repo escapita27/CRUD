e68d9ebf0c9b5fc62823344f7fc8764285c1b48a
escapita27 <129018334+escapita27@users.noreply.github.com>
2024-02-26 17:31:57 -0600

    Add files via upload

---
+++ src/main/java/com/api/crud/services/UserService.java
@@ -0,0 +1,55 @@
+package com.api.crud.services;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.api.crud.models.UserModel;
+import com.api.crud.repositories.IUserRepository;
+
+@Service
+public class UserService{
+
+    @Autowired
+    IUserRepository userRepository;
+
+    public ArrayList<UserModel> getUsers(){
+        return (ArrayList<UserModel>) userRepository.findAll();
+    }
+
+    @SuppressWarnings("null")
+    public UserModel saveUser(UserModel user){
+         return userRepository.save(user);
+    }
+
+    @SuppressWarnings("null")
+    public Optional<UserModel> getById(Long id){
+        return userRepository.findById(id);
+    }
+
+    public UserModel updateById(UserModel request, Long id){
+        UserModel user = userRepository.findById(id).get();
+
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setEmail(request.getEmail());
+
+        return user; 
+    }
+
+    public boolean deleteUser(Long id){
+
+        try{
+            userRepository.deleteById(id);
+            return true;
+
+        }catch(Exception e){
+            return false;
+        }
+    }
+    
+
+
+}
